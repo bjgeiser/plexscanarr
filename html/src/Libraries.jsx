95aeae19724ec6042ffac399795034864e65d556
Bryce Geiser <bryce@bright.ai>
2025-02-11 11:19:55 -0600

    feat: hook up stop library scan

--- html/src/Libraries.jsx
+++ html/src/Libraries.jsx
@@ -56,6 +56,19 @@ function Libraries({ library_in, scanActive }) {
       });
   };
 
+  const handleCanelScanClick = (library) => {
+    console.log("Cancel clicked for", library);
+
+    fetch(`${REST_URL}plex/libraries/${library.key}/scan`, { method: "DELETE" })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Scan canceled:", data);
+        })
+        .catch((error) => {
+          console.error("Error starting scan:", error);
+        });
+  };
+
   const handleLibraryDetailClick = (library) => {
     console.log("Detail clicked for", library);
     navigate(toRoutePath(library.name));
@@ -116,7 +129,7 @@ function Libraries({ library_in, scanActive }) {
               return (
                 <tr key={library.key}>
                   <td>
-                    <div className="dropdown font-bold">
+                    <div className="dropdown dropdown-hover font-bold">
                       <div tabIndex={0} role="button" className="">
                         {library.name}
                       </div>
@@ -144,15 +157,20 @@ function Libraries({ library_in, scanActive }) {
                   </td>
                   <td>
                     {library.scan_active ? (
-                      <div>
-                        <div id={"active_" + library.key + "_scanning"} className="text-sm font-bold text-orange-600">
-                          Scanning
+
+                        <div className="dropdown dropdown-hover">
+                          <div tabIndex={0} role="button" id={"active_" + library.key + "_scanning"} className="text-sm font-bold text-orange-600">
+                            Scanning
+                          </div>
+                          <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                            <li>
+                              <button id={"active_" + library.key + "_stop_scanning"} onClick={() => handleCanelScanClick(library)} className="text-sm font-bold">
+                                Stop
+                              </button>
+                            </li>
+                          </ul>
                         </div>
-                        <button id={"active_" + library.key + "_stop_scanning"} className="text-sm font-bold">
-                          Stop
-                        </button>
-                      </div>
-                    ) : (
+                     ) : (
                       <button id={"active_" + library.key + "_not_scanning"} className="text-sm font-bold" onClick={() => handleScanClick(library)}>
                         Scan
                       </button>
