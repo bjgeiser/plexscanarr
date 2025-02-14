c7affbd65e48c117107281bc61160525285b56cb
Ryan <ryan@bright.ai>
2025-02-14 06:56:43 -0600

    fix: build

--- address-book/vite.config.ts
+++ address-book/vite.config.ts
@@ -1,9 +1,10 @@
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import dotenv from 'dotenv';
+import { cjsInterop } from 'vite-plugin-cjs-interop';
 
 dotenv.config({ path: './.env' });
 
 export default defineConfig({
-  plugins: [reactRouter()],
+  plugins: [reactRouter(), cjsInterop({ dependencies: ['react-use-websocket'] })],
 });
