705a02cbb233c22b4822e68034e45cd4f8c65ba1
Ryan <ryan@bright.ai>
2025-02-14 10:42:06 -0600

    feat: plumb in websockets to home page

--- address-book/app/modules/PlexMessageContext.tsx
+++ address-book/app/modules/PlexMessageContext.tsx
@@ -1,15 +1,38 @@
 import React, { createContext, useEffect, useState, useContext, type ReactNode } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import { getBaseUrl } from '../datalayer';
+import type { A } from 'node_modules/react-router/dist/development/route-data-Cq_b5feC.mjs';
 
-type PlexMessageNotificationType = {
+enum ArrSource {
+  PLEXSCANARR = 'PLEXSCANARR',
+  RADARR = 'RADARR',
+  SONARR = 'SONARR',
+  LIDARR = 'LIDARR',
+  READARR = 'READARR',
+  BAZARR = 'BAZARR',
+  PLEX = 'PLEX',
+}
+
+export type PlexMessageNotificationType = {
+  type: ArrSource;
   arr_type: string;
-  // TODO add
+  timestamp: Date;
+  cover_art_url?: string;
+  pretty_name: string;
+  server_name: string;
+  file_path?: string;
+  original_json: string;
+  scan_started: boolean;
+  release_title?: string;
+  file_size?: string;
+  service_link?: string;
+  content_link?: string;
 };
 export type PlexMessageContextType = {
   notification: PlexMessageNotificationType;
   type: string;
   uuid: string;
+  index?: number;
 };
 
 // Create the context
@@ -19,7 +42,13 @@ const PlexMessageContext = createContext<{
 }>({
   plexMessage: {
     notification: {
+      type: ArrSource.PLEXSCANARR,
       arr_type: 'initial',
+      timestamp: new Date(),
+      pretty_name: 'initial',
+      server_name: 'initial',
+      original_json: 'initial',
+      scan_started: false,
     },
     type: 'initial',
     uuid: 'initial-uuid',
@@ -33,7 +62,13 @@ const PlexMessageContext = createContext<{
 export const PlexMessageProvider = ({ children }: { children: ReactNode }) => {
   const [plexMessage, setPlexMessage] = useState<PlexMessageContextType>({
     notification: {
+      type: ArrSource.PLEXSCANARR,
       arr_type: 'initial',
+      timestamp: new Date(),
+      pretty_name: 'initial',
+      server_name: 'initial',
+      original_json: 'initial',
+      scan_started: false,
     },
     type: 'initial',
     uuid: 'initial-uuid',
@@ -51,7 +86,19 @@ export const PlexMessageProvider = ({ children }: { children: ReactNode }) => {
       const messageData = JSON.parse(lastMessage.data);
       const newMessage: PlexMessageContextType = {
         notification: {
+          type: messageData.notification.type,
           arr_type: messageData.notification.arr_type,
+          timestamp: new Date(messageData.notification.timestamp),
+          cover_art_url: messageData.notification.cover_art_url,
+          pretty_name: messageData.notification.pretty_name,
+          server_name: messageData.notification.server_name,
+          file_path: messageData.notification.file_path,
+          original_json: messageData.notification.original_json,
+          scan_started: messageData.notification.scan_started,
+          release_title: messageData.notification.release_title,
+          file_size: messageData.notification.file_size,
+          service_link: messageData.notification.service_link,
+          content_link: messageData.notification.content_link,
         },
         type: messageData.type,
         uuid: messageData.uuid,
--- address-book/app/routes/home.tsx
+++ address-book/app/routes/home.tsx
@@ -1,12 +1,107 @@
-import { use, useEffect } from 'react';
-import { usePlexMessage } from '../modules/PlexMessageContext';
+import { useEffect, useRef, useState } from 'react';
+import { usePlexMessage, type PlexMessageContextType } from '../modules/PlexMessageContext';
+import Notification from '../layouts/Notification';
 
 export default function Home() {
   const { plexMessage, setPlexMessage } = usePlexMessage();
+  const logIndexRef = useRef(0);
+  const [messageHistory, setMessageHistory] = useState<PlexMessageContextType[]>([]);
 
   useEffect(() => {
     console.log('Home useEffect', plexMessage);
+    if (plexMessage != null) {
+      try {
+        console.log('home Rx: ', plexMessage);
+        if (plexMessage.hasOwnProperty('type')) {
+          const type = plexMessage.type;
+          const notification = plexMessage.notification;
+
+          // if (plexMessage['type'] === 'plex_event') {
+          //   setScanStatus(notification.pretty_name);
+          // }
+
+          if (type === 'arr_event' || type === 'plex_event') {
+            logIndexRef.current += 1;
+            plexMessage.index = logIndexRef.current;
+            setMessageHistory((history) => {
+              if (
+                (history.length === 1 && history[0].notification.pretty_name === 'Welcome to Plexscanarr') ||
+                notification['pretty_name'] === 'Welcome to Plexscanarr'
+              ) {
+                history.length = 0;
+              }
+              while (history.length > 500) {
+                // Drop last message to reduce size by 1
+                history.pop();
+              }
+              return [plexMessage, ...history];
+            });
+          }
+          /*else if (msgJson["type"] === "progress") {
+            if (msgJson["params"]["id"] === "flash_progress") {
+              setProgress(msgJson["params"]["value"]);
+            }
+          } else if (msgJson["type"] === "label") {
+            if (msgJson["params"]["id"] === "device_info") {
+              setDeviceInfo(msgJson["params"]["value"]);
+            } else if (msgJson["params"]["id"] === "test_title") {
+              setTestTile(msgJson["params"]["value"]);
+            } else if (msgJson["params"]["id"] === "available_records") {
+              setAvailableRecords(msgJson["params"]["value"]);
+            } else if (msgJson["params"]["id"] === "test_result") {
+              const val = msgJson["params"]["value"];
+              setTestResult(val);
+              if (val === "Success") {
+                setTestResultBadge("badge badge-success");
+              } else if (val === "Failed" || val === "Exception") {
+                setTestResultBadge("badge badge-error");
+              } else {
+                setTestResultBadge("badge badge-outline");
+              }
+            } else if (msgJson["params"]["id"] === "customer_code") {
+              setCustomerCode(msgJson["params"]["value"]);
+            } else if (msgJson["params"]["id"] === "product_id") {
+              setProductID(msgJson["params"]["value"]);
+            } else if (msgJson["params"]["id"] === "region") {
+              setRegion(msgJson["params"]["value"]);
+            } else if (msgJson["params"]["id"] === "major_revision") {
+              setMajorRevision(msgJson["params"]["value"]);
+            } else if (msgJson["params"]["id"] === "manufacturer") {
+              setManufacturer(msgJson["params"]["value"]);
+            } else if (msgJson["params"]["id"] === "tester_id") {
+              setTesterID(msgJson["params"]["value"]);
+            } else if (msgJson["params"]["id"] === "full_serial_number") {
+              setFullSerialNUmber(msgJson["params"]["value"]);
+            } else if (msgJson["params"]["id"] === "server_connection_status") {
+              setServerConnectionStatus(msgJson["params"]["value"]);
+            }
+          } else if (msgJson["type"] === "disabled") {
+            if (msgJson["params"]["id"] === "start_btn") {
+              setStartBtnDisabled(msgJson["params"]["value"]);
+            } else if (msgJson["params"]["id"] === "reprint_btn") {
+              setReprintBtnDisabled(msgJson["params"]["value"]);
+            }
+          }*/
+        }
+      } catch (e) {
+        //do nothing
+      }
+    }
   }, [plexMessage]);
 
-  return <p>{plexMessage ? plexMessage.notification.arr_type : 'None'}</p>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <div>
+        <h2>WebSocket Messages</h2>
+        <ul>
+          {messageHistory.map((message, index) => (
+            <li key={index}>
+              <Notification notification={message.notification} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
