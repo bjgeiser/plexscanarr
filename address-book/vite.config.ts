0e980fa6b5f7b9610d7396d50065cd9483e3ebe0
Ryan <ryan@bright.ai>
2025-02-15 10:03:52 -0600

    refactor: finally back to sane css

--- address-book/vite.config.ts
+++ address-book/vite.config.ts
@@ -2,10 +2,9 @@ import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import dotenv from 'dotenv';
 import { cjsInterop } from 'vite-plugin-cjs-interop';
-import tailwindcss from '@tailwindcss/vite';
 
 dotenv.config({ path: './.env' });
 
 export default defineConfig({
-  plugins: [reactRouter(), cjsInterop({ dependencies: ['react-use-websocket'] }), tailwindcss()],
+  plugins: [reactRouter(), cjsInterop({ dependencies: ['react-use-websocket'] })],
 });
