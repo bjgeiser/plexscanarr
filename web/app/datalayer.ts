7815aedb5701dce5658153f830adc111c2db8cc4
Bryce Geiser <bryce@bright.ai>
2025-02-18 00:51:06 -0600

    fix: make backend detection and env override

--- web/app/datalayer.ts
+++ web/app/datalayer.ts
@@ -1,4 +1,5 @@
 import { createDataLayerService } from './services/DataLayer';
+import * as process from 'node:process';
 
 export function getBaseUrl(): {
   plexUrl: URL;
@@ -6,9 +7,40 @@ export function getBaseUrl(): {
 } {
   const plexIp = 'localhost';
   const plexPort = '5002';
+  var rest_url = ""
+  var ws_url = ""
+
+
+  if(document.location.protocol === 'https:')
+  {
+      rest_url = document.location.protocol + "//" + document.location.host + "/";
+      ws_url =  "wss://" + document.location.host + "/ws";
+  }
+  else if(document.location.protocol === 'http:')
+  {
+      rest_url = document.location.protocol + "//" + document.location.host + "/";
+      ws_url =  "ws://" + document.location.host + "/ws";
+  }
+  else if(import.meta.env.VITE_WEB_SERVER_ADDR !== undefined)
+  {
+    rest_url =  `http://${import.meta.env.VITE_WEB_SERVER_ADDR}:${import.meta.env.VITE_WEB_SERVER_PORT}/`;
+    ws_url =  `ws://${import.meta.env.VITE_WEB_SERVER_ADDR}:${import.meta.env.VITE_WEB_SERVER_PORT}/ws`;
+  }
+  else if(document.location.protocol === 'file:')
+  {
+      rest_url =  `http://${plexIp}:${plexPort}/`;
+      ws_url =  `ws://${plexIp}:${plexPort}/ws`;
+  }
+
+  if(import.meta.env.VITE_FORCE_ENV)
+  {
+    rest_url =  `http://${import.meta.env.VITE_WEB_SERVER_ADDR}:${import.meta.env.VITE_WEB_SERVER_PORT}/`;
+    ws_url =  `ws://${import.meta.env.VITE_WEB_SERVER_ADDR}:${import.meta.env.VITE_WEB_SERVER_PORT}/ws`;
+  }
+
   // const plexIp = process.env.PLEX_IP || 'localhost';
   // const plexPort = process.env.PLEX_PORT || '5000';
-  return { plexUrl: new URL(`http://${plexIp}:${plexPort}/`), plexWS: new URL(`ws://${plexIp}:${plexPort}/ws`) };
+  return { plexUrl: new URL(rest_url), plexWS: new URL(ws_url) };
 }
 
 export const datalayer = createDataLayerService();
